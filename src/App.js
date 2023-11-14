29e0e99b46e6bdb0b8570fd1c78d0618b929741f
Mariano0o92 <mariuszn13@gmail.com>
2023-11-14 17:44:03 +0100

    Conversion from JS to React App was fully done.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-import Header from './components/Header';
-import Balance from './components/Balance';
-import IncomeExpenses from './components/IncomeExpenses';
-import TransactionList from './components/TransactionList';
-import AddTransaction from './components/AddTransaction';
+import './App.css'
+import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
+import { GlobalProvider } from './context/GlobalState'
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <div className='container'></div>
-      <Balance />
-      <IncomeExpenses />
-      <TransactionList />
-      <AddTransaction />
-    </div>
-  );
-  }
+	return (
+		<GlobalProvider>
+			<Header />
+			<div className='container'></div>
+			<Balance />
+			<IncomeExpenses />
+			<TransactionList />
+			<AddTransaction />
+		</GlobalProvider>
+	)
+}
 
-export default App;
+export default App
