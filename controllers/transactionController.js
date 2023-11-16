678400966aebb923244c851eecb116bed0cd47cd
Mariano0o92 <mariuszn13@gmail.com>
2023-11-16 11:02:40 +0100

    Get, add and delete transactions are now working.

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -55,5 +55,25 @@ return res.status(400).json({
 // @route DELETE /api/v1/transactions/:id
 // @access Public
 exports.deleteTransaction = async (req, res, next) => {
-    res.send('DELETE transaction')
+    try {
+        const transaction = await Transaction.findById(req.params.id)
+        if(!transaction) {
+            return res.status(404).json({
+                success: false,
+                error: 'No transaction found'
+            })
+        }
+
+        await transaction.deleteOne()
+
+        return res.status(200).json({
+            success: true,
+            data: {}
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        })
+    }
 }
